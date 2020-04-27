5a1091d02e2c01af1d9a037cefd3947525e62e0a
Ash <sayhi@ashryan.io>
2020-04-27 14:27:36 -0400

    Show library contents.

--- expressjs/public/javascripts/cc-libraries.js
+++ expressjs/public/javascripts/cc-libraries.js
@@ -6,8 +6,23 @@ getLibrariesButton.addEventListener("click", async () => {
   const json = await response.json();
 
   const libraryNamesList = json.libraries
-    .map((library) => `<li>${library.name}</li>`)
+    .map(
+      (library) =>
+        `<li data-libs-id="${library.id}">${library.name} <button onclick="showLibraryContent('${library.id}')">Show contents</button></li>`
+    )
     .join("");
 
   responsesDiv.innerHTML = `<ul>${libraryNamesList}</ul>`;
 });
+
+const showLibraryContent = async (id) => {
+  const response = await fetch(`/cc-libraries/${id}`);
+  const json = await response.json();
+
+  const libraryElementsList = json.elements
+    .map((element) => `<li>${element.name}</li>`)
+    .join("");
+
+  const parentLibary = document.querySelector(`[data-libs-id="${id}"]`);
+  parentLibary.innerHTML += `<ul>${libraryElementsList}</ul>`;
+};
--- expressjs/routes/index.js
+++ expressjs/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const baseURL = "https://libraries.adobe.io/api/v1/libraries/";
+const baseURL = "https://libraries.adobe.io/api/v1/libraries";
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
@@ -22,7 +22,26 @@ router.get("/cc-libraries", async (req, res, next) => {
     const response = await axios.get(baseURL, options);
     res.json(response.data);
   } catch (error) {
-    console.log(error.response.data, error.config);
+    console.log(error);
+  }
+});
+
+router.get("/cc-libraries/:id", async (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+
+  const options = {
+    headers: {
+      "x-api-key": process.env.API_KEY,
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await axios.get(`${baseURL}/${id}/elements`, options);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
   }
 });
 
