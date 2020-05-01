9f0fd76145c4f2b5f54f0d8c61a37ee498cdfe92
Ash <sayhi@ashryan.io>
2020-05-01 17:04:42 -0400

    Basic tests.

---
+++ getting-information-nodejs/test/routes/index.test.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { expect } = chai;
+const server = require("../../app");
+
+chai.use(chaiHttp);
+
+describe("GET /", () => {
+  it("Returns the homepage", (done) => {
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.contain(
+          `<button id="get-libraries">Get CC Libraries</button>`
+        );
+        done();
+      });
+  });
+});
+
+describe("GET /cc-libraries", () => {
+  it("Returns JSON data describing the user's libraries", (done) => {
+    chai
+      .request(server)
+      .get("/cc-libraries")
+      .set("x-api-key", process.env.API_KEY)
+      .set("Authorization", `Bearer ${process.env.ACCESS_TOKEN}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.a.property("total_count").that.is.a("number");
+        expect(res.body).to.have.a.property("libraries").that.is.a("array");
+        done();
+      });
+  });
+});
